635ec9d76b1a9ae461b5e2d6e31b05b08d7d8abd
Muhammed Shabeeb <muhammedshabeeb330@gmail.com>
2024-07-01 09:48:59 +0530

    local

--- utils/passport.js
+++ utils/passport.js
@@ -13,7 +13,7 @@ passport.deserializeUser((user, done) => {
 passport.use(new GoogleStrategy({
     clientID:     '124777832788-if7u3lre02nlcvfsesjr7jmdijtmn1nk.apps.googleusercontent.com',
     clientSecret: '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',
-    callbackURL: "https://anfieldturfbookings.online/auth/google/callback",
+    callbackURL: "http://localhost:8000/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
